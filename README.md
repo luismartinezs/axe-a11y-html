b79933826a3e1035bf990de99648f5139f1f82c7
Luis Martinez <luismartinezwebdev@gmail.com>
2022-09-06 10:01:02 +0200

    chore: first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Axe cypress a11y html
+
+Simple example of a11y e2e testing with cypress and axe
---
+++ cypress/e2e/a11y.cy.js
@@ -0,0 +1,9 @@
+describe("Accessibility tests", () => {
+  beforeEach(() => {
+      cy.visit("/").get("main")
+      cy.injectAxe()
+  })
+  it("Has no detectable accessibility violations on load", () => {
+    cy.checkA11y()
+  })
+})
